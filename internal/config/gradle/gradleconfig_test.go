b6f739e02ada795293d76ce6f1b5615e9c4b0d6f
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2024-02-19 16:20:02 +0100

    chore: Bump gradle-analytics to 2.0.2

--- internal/config/gradle/gradleconfig_test.go
+++ internal/config/gradle/gradleconfig_test.go
@@ -111,7 +111,7 @@ initscript {
     }
     dependencies {
         classpath("io.bitrise.gradle:remote-cache:1.+")
-        classpath("io.bitrise.gradle:gradle-analytics:2.0.1")
+        classpath("io.bitrise.gradle:gradle-analytics:2.0.2")
     }
 }
 
@@ -165,7 +165,7 @@ initscript {
     }
     dependencies {
         classpath("io.bitrise.gradle:remote-cache:1.+")
-        classpath("io.bitrise.gradle:gradle-analytics:2.0.1")
+        classpath("io.bitrise.gradle:gradle-analytics:2.0.2")
     }
 }
 
