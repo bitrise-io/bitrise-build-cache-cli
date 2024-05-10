138f864a5d3fec5bce82e569ef5c9edc4e523421
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2024-05-10 18:33:01 +0200

    fix: ACI-2098 Have to name gradle init script .init.gradle.kts

--- cmd/enableForGradle.go
+++ cmd/enableForGradle.go
@@ -24,7 +24,7 @@ var enableForGradleCmd = &cobra.Command{ //nolint:gochecknoglobals
 	Long: `Enable Bitrise Build Cache for Gradle.
 This command will:
 
-- Create a ~/.gradle/init.d/bitrise-build-cache-init.gradle.kts file with the necessary configs. This file will be overwritten.
+- Create a ~/.gradle/init.d/bitrise-build-cache.init.gradle.kts file with the necessary configs. This file will be overwritten.
 - Create a ~/.gradle/gradle.properties file with org.gradle.caching=true
 
 The gradle.properties file will be created if it doesn't exist.
@@ -85,18 +85,18 @@ func enableForGradleCmdFn(logger log.Logger, gradleHomePath string, envProvider 
 		return fmt.Errorf("ensure ~/.gradle/init.d exists: %w", err)
 	}
 
-	logger.Infof("(i) Generate ~/.gradle/init.d/bitrise-build-cache-init.gradle.kts")
+	logger.Infof("(i) Generate ~/.gradle/init.d/bitrise-build-cache.init.gradle.kts")
 	initGradleContent, err := gradleconfig.GenerateInitGradle(endpointURL, authToken, paramIsGradleMetricsEnabled, cacheConfigMetadata)
 	if err != nil {
-		return fmt.Errorf("generate bitrise-build-cache-init.gradle.kts: %w", err)
+		return fmt.Errorf("generate bitrise-build-cache.init.gradle.kts: %w", err)
 	}
 
-	logger.Infof("(i) Write ~/.gradle/init.d/bitrise-build-cache-init.gradle.kts")
+	logger.Infof("(i) Write ~/.gradle/init.d/bitrise-build-cache.init.gradle.kts")
 	{
-		initGradlePath := filepath.Join(gradleInitDPath, "bitrise-build-cache-init.gradle.kts")
+		initGradlePath := filepath.Join(gradleInitDPath, "bitrise-build-cache.init.gradle.kts")
 		err = os.WriteFile(initGradlePath, []byte(initGradleContent), 0755) //nolint:gosec,gomnd,mnd
 		if err != nil {
-			return fmt.Errorf("write bitrise-build-cache-init.gradle.kts to %s, error: %w", initGradlePath, err)
+			return fmt.Errorf("write bitrise-build-cache.init.gradle.kts to %s, error: %w", initGradlePath, err)
 		}
 	}
 
