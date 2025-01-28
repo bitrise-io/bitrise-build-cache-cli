b682de687a596292d39e0b0b76598d21417e5535
Marcell Baan <marcell.baan@bitrise.io>
2025-01-28 13:17:36 +0000

    feat: ACI-3319 Switch IAD and ORD to TLS

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -2,13 +2,13 @@ package consts
 
 const (
 	EndpointURLDefault = "grpcs://remote-build-cache.services.bitrise.io"
-	EndpointURLLAS1    = "grpc://las-cache.services.bitrise.io:6666"
-	EndpointURLATL1    = "grpc://atl-cache.services.bitrise.io:6666"
+	EndpointURLLAS1    = "grpc://las-cache.services.bitrise.io:6666" ord
+	EndpointURLATL1    = "grpc://atl-cache.services.bitrise.io:6666" iad
 	EndpointURLIAD1    = "grpcs://iad-cache.services.bitrise.io:443"
 	EndpointURLORD1    = "grpcs://ord-cache.services.bitrise.io:443"
 
-	RBEEndpointURLIAD1 = "grpcs://rbe-internal-iad.services:6669"
-	RBEEndpointURLORD1 = "grpcs://rbe-internal-ord.services:6669"
+	RBEEndpointURLIAD1 = "grpcs://rbe-internal-iad.services.bitrise.io:6669"
+	RBEEndpointURLORD1 = "grpcs://rbe-internal-ord.services.bitrise.io:6669"
 
 	AnalyticsServiceEndpoint = "https://xcode-analytics.services.bitrise.io"
 
