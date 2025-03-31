15f4c4b73225e5adf98176a856c66c4df926732a
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2025-03-31 11:30:20 +0200

    feat: ACI-3563 collect host cache config metadata

--- internal/config/common/cache_config_test.go
+++ internal/config/common/cache_config_test.go
@@ -2,12 +2,16 @@ package common
 
 import (
 	"reflect"
+	"slices"
+	"strings"
 	"testing"
 
 	"github.com/bitrise-io/go-utils/v2/log"
 )
 
 func TestNewCacheConfigMetadata(t *testing.T) {
+	t.Parallel()
+
 	type args struct {
 		envProvider EnvProviderFunc
 		commandFunc CommandFunc
@@ -82,9 +86,120 @@ func TestNewCacheConfigMetadata(t *testing.T) {
 				RepoURL:    "https://github.com/owner/repo",
 			},
 		},
+		{
+			name: "OS",
+			args: args{
+				envProvider: createEnvProvider(map[string]string{
+					"BITRISE_IO": "true",
+				}),
+				commandFunc: func(c string, _ ...string) (string, error) {
+					if strings.Contains(c, "uname") {
+						return "Linux", nil
+					}
+
+					return "", nil
+				},
+			},
+			want: CacheConfigMetadata{
+				CIProvider: CIProviderBitrise,
+				HostMetadata: HostMetadata{
+					OS: "Linux",
+				},
+			},
+		},
+		{
+			name: "Non-CI OS",
+			args: args{
+				envProvider: createEnvProvider(map[string]string{}),
+				commandFunc: func(c string, _ ...string) (string, error) {
+					if strings.Contains(c, "uname") {
+						return "Linux", nil
+					}
+
+					return "", nil
+				},
+			},
+			want: CacheConfigMetadata{
+				HostMetadata: HostMetadata{
+					OS: "",
+				},
+			},
+		},
+		{
+			name: "Locale",
+			args: args{
+				envProvider: createEnvProvider(map[string]string{
+					"BITRISE_IO": "true",
+					"LANG":       "en_US.UTF-8",
+				}),
+				commandFunc: func(_ string, _ ...string) (string, error) {
+					return "", nil
+				},
+			},
+			want: CacheConfigMetadata{
+				CIProvider: CIProviderBitrise,
+				HostMetadata: HostMetadata{
+					Locale:         "en_US",
+					DefaultCharset: "UTF-8",
+				},
+			},
+		},
+		{
+			name: "CPU",
+			args: args{
+				envProvider: createEnvProvider(map[string]string{
+					"BITRISE_IO": "true",
+				}),
+				commandFunc: func(c string, a ...string) (string, error) {
+					if strings.Contains(c, "nproc") ||
+						(strings.Contains(c, "sysctl") && slices.Contains(a, "hw.ncpu")) {
+						return "4", nil
+					}
+
+					return "", nil
+				},
+			},
+			want: CacheConfigMetadata{
+				CIProvider: CIProviderBitrise,
+				HostMetadata: HostMetadata{
+					CPUCores: 4,
+				},
+			},
+		},
+		{
+			name: "Memory",
+			args: args{
+				envProvider: createEnvProvider(map[string]string{
+					"BITRISE_IO": "true",
+				}),
+				commandFunc: func(c string, a ...string) (string, error) {
+					hasMemTotal := slices.ContainsFunc(a, func(s string) bool {
+						return strings.Contains(s, "MemTotal")
+					})
+					hasMemSize := strings.Contains(c, "sysctl") && slices.Contains(a, "hw.memsize")
+
+					if hasMemTotal {
+						return "1", nil
+					}
+					if hasMemSize {
+						return "1000", nil
+					}
+
+					return "", nil
+				},
+			},
+			want: CacheConfigMetadata{
+				CIProvider: CIProviderBitrise,
+				HostMetadata: HostMetadata{
+					MemSize: 1000,
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
 			if got := NewCacheConfigMetadata(tt.args.envProvider,
 				tt.args.commandFunc,
 				log.NewLogger()); !reflect.DeepEqual(got, tt.want) {
