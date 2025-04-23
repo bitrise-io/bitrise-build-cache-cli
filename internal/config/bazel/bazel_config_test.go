66a5954ecdd5db11cc3aedd34b4a5ae653f36af8
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2025-04-23 16:52:48 +0200

    feat: ACI-3637 Add timestamps flag for bazel

--- internal/config/bazel/bazel_config_test.go
+++ internal/config/bazel/bazel_config_test.go
@@ -17,6 +17,7 @@ func TestGenerateBazelrc(t *testing.T) {
 		rbeEndpointURL      string
 		workspaceID         string
 		authToken           string
+		timestamps          bool
 		cacheConfigMetadata common.CacheConfigMetadata
 	}
 	tests := []struct {
@@ -154,12 +155,38 @@ build --bes_header='x-cpu-cores=4'
 build --bes_header='x-mem-size=1000'
 `,
 		},
+		{
+			name: "With timestamps",
+			args: args{
+				endpointURL: "grpcs://TESTENDPOINT.bitrise.io",
+				authToken:   "4uth70k3n",
+				timestamps:  true,
+			},
+			wantErr: "",
+			want: `build --remote_cache=grpcs://TESTENDPOINT.bitrise.io
+build --remote_timeout=600s
+build --bes_timeout=2m
+build --show_timestamps
+build --remote_header=authorization="Bearer 4uth70k3n"
+build --bes_header=authorization="Bearer 4uth70k3n"
+build --remote_header=x-flare-buildtool=bazel
+build --remote_header=x-flare-builduser=
+build --bes_results_url=https://app.bitrise.io/build-cache/invocations/bazel/
+build --bes_backend=grpcs://flare-bes.services.bitrise.io:443
+build --build_event_publish_all_actions
+`,
+		},
 	}
 	for _, tt := range tests { //nolint:varnamelen
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			got, err := GenerateBazelrc(tt.args.endpointURL, tt.args.rbeEndpointURL, tt.args.workspaceID, tt.args.authToken, tt.args.cacheConfigMetadata)
+			got, err := GenerateBazelrc(tt.args.endpointURL, tt.args.workspaceID, tt.args.authToken,
+				tt.args.cacheConfigMetadata,
+				Preferences{
+					RBEEndpointURL:      tt.args.rbeEndpointURL,
+					IsTimestampsEnabled: tt.args.timestamps,
+				})
 
 			if tt.wantErr != "" {
 				require.EqualError(t, err, tt.wantErr)
