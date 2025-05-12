f28592cfa1eec316ff95d1144cd5f5503fcb905a
Balázs Hajagos <balazs.hajagos@bitrise.io>
2025-05-12 10:16:53 +0200

    Add os to gradle configuration cache key
    This is to keep fallback in multiplatform projects to the same os we are trying to build on

--- internal/gradle/key.go
+++ internal/gradle/key.go
@@ -1,6 +1,9 @@
 package gradle
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 type CacheKeyParams struct {
 	IsFallback bool
@@ -17,9 +20,11 @@ func (g *Cache) GetCacheKey(keyParams CacheKeyParams) (string, error) {
 		return "", fmt.Errorf("cache key is required if BITRISE_APP_SLUG env var is not set")
 	}
 
+	os := runtime.GOOS
+
 	if keyParams.IsFallback {
-		return fmt.Sprintf("gradle-config-cache-metadata-%s", appSlug), nil
+		return fmt.Sprintf("gradle-config-cache-metadata-%s-%s", appSlug, os), nil
 	}
 
-	return fmt.Sprintf("gradle-config-cache-metadata-%s-%s", appSlug, branch), nil
+	return fmt.Sprintf("gradle-config-cache-metadata-%s-%s-%s", appSlug, branch, os), nil
 }
