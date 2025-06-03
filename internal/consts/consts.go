c84ebf3bf1ba85aadaa39a5ede6884e3d40eaede
Bitrise CI <noreply@bitrise.io>
2025-06-03 09:59:22 +0000

    feat: update plugins to release

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -26,17 +26,17 @@ const (
 	AnalyticsServiceEndpoint = "https://xcode-analytics.services.bitrise.io"
 
 	// Gradle Remote Build Cache related consts
-	GradleRemoteBuildCachePluginDepVersion = "1.2.18"
+	GradleRemoteBuildCachePluginDepVersion = "1.2.19"
 
 	// Gradle Analytics related consts
-	GradleAnalyticsPluginDepVersion = "2.1.25"
+	GradleAnalyticsPluginDepVersion = "2.1.28"
 	GradleAnalyticsEndpoint         = "gradle-analytics.services.bitrise.io"
 	GradleAnalyticsHTTPEndpoint     = "https://gradle-sink.services.bitrise.io"
 	GradleAnalyticsPort             = 443
 
 	// Gradle Common Plugin version
-	GradleCommonPluginDepVersion = "0.0.1"
+	GradleCommonPluginDepVersion = "1.0.1"
 
 	// Gradle Test Distribution Plugin version
-	GradleTestDistributionPluginDepVersion = "0.0.1"
+	GradleTestDistributionPluginDepVersion = "2.1.24"
 )
