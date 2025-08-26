1d2f02e471d104ac587baef7fec038aeab9c5af9
balazs.benyo <balazs.benyo@bitrise.io>
2025-08-26 13:24:36 +0200

    feat: ACI-3989 collect cache hit rate

--- internal/xcelerate/proxy/proxy.go
+++ internal/xcelerate/proxy/proxy.go
@@ -37,7 +37,7 @@ type Proxy struct {
 	sessionMutex       sync.Mutex
 	capabilitiesCalled bool
 
-	stats *stats
+	statsCollector *statsCollector
 
 	logger log.Logger
 }
@@ -48,9 +48,9 @@ func NewProxy(
 ) *grpc.Server {
 	//nolint:exhaustruct
 	proxy := &Proxy{
-		kvClient: kvClient,
-		stats:    newStats(),
-		logger:   logger,
+		kvClient:       kvClient,
+		statsCollector: newStatsCollector(),
+		logger:         logger,
 	}
 
 	grpcServer := grpc.NewServer(
@@ -85,7 +85,7 @@ func (p *Proxy) SetSession(_ context.Context, request *session.SetSessionRequest
 
 	p.kvClient.ChangeSession(request.GetInvocationId(), request.GetAppSlug(), request.GetBuildSlug(), request.GetStepSlug())
 
-	p.stats = newStats()
+	p.statsCollector = newStatsCollector()
 
 	p.logger.TInfof("SetSession called with invocation ID: %s, app slug: %s, build slug: %s, step slug: %s",
 		request.GetInvocationId(),
@@ -98,11 +98,13 @@ func (p *Proxy) SetSession(_ context.Context, request *session.SetSessionRequest
 }
 
 func (p *Proxy) GetSessionStats(_ context.Context, _ *emptypb.Empty) (*session.GetSessionStatsResponse, error) {
-	downloadBytes, uploadBytes := p.stats.getBytes()
+	collectedStats := p.statsCollector.getStats()
 
 	return &session.GetSessionStatsResponse{
-		UploadedBytes:   uploadBytes,
-		DownloadedBytes: downloadBytes,
+		UploadedBytes:   collectedStats.uploadBytes,
+		DownloadedBytes: collectedStats.downloadBytes,
+		Hits:            collectedStats.hits,
+		Misses:          collectedStats.misses,
 	}, nil
 }
 
@@ -113,7 +115,7 @@ func (p *Proxy) Get(ctx context.Context, request *llvmcas.CASGetRequest) (*llvmc
 
 	errorHandler := func(err error) *llvmcas.CASGetResponse {
 		if errors.Is(err, kv.ErrCacheNotFound) {
-			p.logger.TInfof("miss")
+			p.statsCollector.addMiss(key)
 
 			//nolint:exhaustruct
 			return &llvmcas.CASGetResponse{
@@ -137,7 +139,8 @@ func (p *Proxy) Get(ctx context.Context, request *llvmcas.CASGetRequest) (*llvmc
 		return errorHandler(fmt.Errorf("failed to download data: %w", err)), nil
 	}
 
-	p.stats.addDownloadBytes(int64(buffer.Len()))
+	p.statsCollector.addHit(key)
+	p.statsCollector.addDownloadBytes(int64(buffer.Len()))
 
 	data := blob{} //nolint:exhaustruct
 	if err := gob.NewDecoder(buffer).Decode(&data); err != nil {
@@ -215,7 +218,7 @@ func (p *Proxy) Put(ctx context.Context, request *llvmcas.CASPutRequest) (*llvmc
 	p.logger.TInfof("Put: CAS ID: %s", key)
 
 	size := int64(buffer.Len())
-	p.stats.addUploadBytes(size)
+	p.statsCollector.addUploadBytes(size)
 
 	err := p.kvClient.UploadStreamToBuildCache(ctx, buffer, key, size)
 	if err != nil {
@@ -236,7 +239,7 @@ func (p *Proxy) Load(ctx context.Context, request *llvmcas.CASLoadRequest) (*llv
 
 	errorHandler := func(err error) *llvmcas.CASLoadResponse {
 		if errors.Is(err, kv.ErrCacheNotFound) {
-			p.logger.TInfof("miss")
+			p.statsCollector.addMiss(key)
 
 			//nolint:exhaustruct
 			return &llvmcas.CASLoadResponse{
@@ -262,7 +265,8 @@ func (p *Proxy) Load(ctx context.Context, request *llvmcas.CASLoadRequest) (*llv
 		return errorHandler(fmt.Errorf("failed to download data: %w", err)), nil
 	}
 
-	p.stats.addDownloadBytes(int64(buffer.Len()))
+	p.statsCollector.addHit(key)
+	p.statsCollector.addDownloadBytes(int64(buffer.Len()))
 
 	data, err := io.ReadAll(buffer)
 	if err != nil {
@@ -343,7 +347,7 @@ func (p *Proxy) Save(ctx context.Context, request *llvmcas.CASSaveRequest) (*llv
 		reader = bytes.NewBuffer(request.GetData().GetBlob().GetData())
 	}
 
-	p.stats.addUploadBytes(size)
+	p.statsCollector.addUploadBytes(size)
 
 	err := p.kvClient.UploadStreamToBuildCache(ctx, reader, key, size)
 	if err != nil {
@@ -364,7 +368,8 @@ func (p *Proxy) GetValue(ctx context.Context, request *llvmkv.GetValueRequest) (
 
 	errorHandler := func(err error) *llvmkv.GetValueResponse {
 		if errors.Is(err, kv.ErrCacheNotFound) {
-			p.logger.TInfof("miss")
+			p.statsCollector.addMiss(key)
+
 			//nolint:exhaustruct
 			return &llvmkv.GetValueResponse{
 				Outcome: llvmkv.GetValueResponse_KEY_NOT_FOUND,
@@ -389,7 +394,8 @@ func (p *Proxy) GetValue(ctx context.Context, request *llvmkv.GetValueRequest) (
 		return errorHandler(fmt.Errorf("failed to download value: %w", err)), nil
 	}
 
-	p.stats.addDownloadBytes(int64(buffer.Len()))
+	p.statsCollector.addHit(key)
+	p.statsCollector.addDownloadBytes(int64(buffer.Len()))
 
 	var entries map[string][]byte
 	if err := gob.NewDecoder(buffer).Decode(&entries); err != nil {
@@ -427,7 +433,7 @@ func (p *Proxy) PutValue(ctx context.Context, request *llvmkv.PutValueRequest) (
 	}
 
 	size := int64(buffer.Len())
-	p.stats.addUploadBytes(size)
+	p.statsCollector.addUploadBytes(size)
 
 	err := p.kvClient.UploadStreamToBuildCache(ctx, buffer, key, size)
 	if err != nil {
--- internal/xcelerate/proxy/stats.go
+++ internal/xcelerate/proxy/stats.go
@@ -1,26 +1,61 @@
 package proxy
 
 import (
+	"sync"
 	"sync/atomic"
 )
 
-type stats struct {
+type statsCollector struct {
 	downloadBytes atomic.Int64
 	uploadBytes   atomic.Int64
+	misses        sync.Map
+	hits          sync.Map
 }
 
-func newStats() *stats {
-	return &stats{}
+type stats struct {
+	downloadBytes int64
+	uploadBytes   int64
+	misses        int64
+	hits          int64
 }
 
-func (s *stats) addDownloadBytes(n int64) {
+func newStatsCollector() *statsCollector {
+	return &statsCollector{}
+}
+
+func (s *statsCollector) addDownloadBytes(n int64) {
 	s.downloadBytes.Add(n)
 }
 
-func (s *stats) addUploadBytes(n int64) {
+func (s *statsCollector) addUploadBytes(n int64) {
 	s.uploadBytes.Add(n)
 }
 
-func (s *stats) getBytes() (int64, int64) {
-	return s.downloadBytes.Load(), s.uploadBytes.Load()
+func (s *statsCollector) getStats() stats {
+	return stats{
+		downloadBytes: s.downloadBytes.Load(),
+		uploadBytes:   s.uploadBytes.Load(),
+		misses:        s.countMap(&s.misses),
+		hits:          s.countMap(&s.hits),
+	}
+}
+
+func (s *statsCollector) addMiss(key string) {
+	s.misses.Store(key, struct{}{})
+}
+
+func (s *statsCollector) addHit(key string) {
+	s.hits.Store(key, struct{}{})
+}
+
+func (s *statsCollector) countMap(m *sync.Map) int64 {
+	var count int64
+
+	m.Range(func(_, _ any) bool {
+		count++
+
+		return true
+	})
+
+	return count
 }
