ab67446a4890ac131c2d97e9fa8ddf29b1f0e582
Balázs Hajagos <balazs.hajagos@bitrise.io>
2025-11-12 11:57:48 +0100

    Make ActivateGradleCmd public

--- cmd/gradle/activate_gradle.go
+++ cmd/gradle/activate_gradle.go
@@ -16,8 +16,8 @@ const (
 	ErrFmtFailedToUpdateProps = `failed to update gradle.properties: %w"`
 )
 
-// activateGradleCmd represents the `gradle` subcommand under `activate`
-var activateGradleCmd = &cobra.Command{ //nolint:gochecknoglobals
+// ActivateGradleCmd represents the `gradle` subcommand under `activate`
+var ActivateGradleCmd = &cobra.Command{ //nolint:gochecknoglobals
 	Use:   "gradle",
 	Short: "Activate Bitrise Plugins for Gradle",
 	Long: `Activate Bitrise Plugins for Gradle.
@@ -74,19 +74,19 @@ If the "# [start/end] generated-by-bitrise-build-cache" block is already present
 var activateGradleParams = gradleconfig.DefaultActivateGradleParams()
 
 func init() {
-	common.ActivateCmd.AddCommand(activateGradleCmd)
-	activateGradleCmd.Flags().BoolVar(&activateGradleParams.Cache.Enabled, "cache", activateGradleParams.Cache.Enabled, "Activate cache plugin. Will override cache-dep.")
-	activateGradleCmd.Flags().BoolVar(&activateGradleParams.Cache.JustDependency, "cache-dep", activateGradleParams.Cache.JustDependency, "Add cache plugin as a dependency only.")
-	activateGradleCmd.Flags().BoolVar(&activateGradleParams.Cache.PushEnabled, "cache-push", activateGradleParams.Cache.PushEnabled, "Push enabled/disabled. Enabled means the build can also write new entries to the remote cache. Disabled means the build can only read from the remote cache.")
-	activateGradleCmd.Flags().StringVar(&activateGradleParams.Cache.ValidationLevel, "cache-validation", activateGradleParams.Cache.ValidationLevel, "Level of cache entry validation for both uploads and downloads. Possible values: none, warning, error")
-	activateGradleCmd.Flags().StringVar(&activateGradleParams.Cache.Endpoint, "cache-endpoint", activateGradleParams.Cache.Endpoint, "The endpoint can be manually provided here for caching operations.")
+	common.ActivateCmd.AddCommand(ActivateGradleCmd)
+	ActivateGradleCmd.Flags().BoolVar(&activateGradleParams.Cache.Enabled, "cache", activateGradleParams.Cache.Enabled, "Activate cache plugin. Will override cache-dep.")
+	ActivateGradleCmd.Flags().BoolVar(&activateGradleParams.Cache.JustDependency, "cache-dep", activateGradleParams.Cache.JustDependency, "Add cache plugin as a dependency only.")
+	ActivateGradleCmd.Flags().BoolVar(&activateGradleParams.Cache.PushEnabled, "cache-push", activateGradleParams.Cache.PushEnabled, "Push enabled/disabled. Enabled means the build can also write new entries to the remote cache. Disabled means the build can only read from the remote cache.")
+	ActivateGradleCmd.Flags().StringVar(&activateGradleParams.Cache.ValidationLevel, "cache-validation", activateGradleParams.Cache.ValidationLevel, "Level of cache entry validation for both uploads and downloads. Possible values: none, warning, error")
+	ActivateGradleCmd.Flags().StringVar(&activateGradleParams.Cache.Endpoint, "cache-endpoint", activateGradleParams.Cache.Endpoint, "The endpoint can be manually provided here for caching operations.")
 
-	activateGradleCmd.Flags().BoolVar(&activateGradleParams.Analytics.Enabled, "analytics", activateGradleParams.Analytics.Enabled, "Activate analytics plugin. Will override analytics-dep.")
-	activateGradleCmd.Flags().BoolVar(&activateGradleParams.Analytics.JustDependency, "analytics-dep", activateGradleParams.Analytics.JustDependency, "Add analytics plugin as a dependency only.")
+	ActivateGradleCmd.Flags().BoolVar(&activateGradleParams.Analytics.Enabled, "analytics", activateGradleParams.Analytics.Enabled, "Activate analytics plugin. Will override analytics-dep.")
+	ActivateGradleCmd.Flags().BoolVar(&activateGradleParams.Analytics.JustDependency, "analytics-dep", activateGradleParams.Analytics.JustDependency, "Add analytics plugin as a dependency only.")
 
-	activateGradleCmd.Flags().BoolVar(&activateGradleParams.TestDistro.Enabled, "test-distribution", activateGradleParams.TestDistro.Enabled, "Activate test distribution plugin for the provided app slug. Will override test-distribution-dep.")
-	activateGradleCmd.Flags().BoolVar(&activateGradleParams.TestDistro.JustDependency, "test-distribution-dep", activateGradleParams.TestDistro.JustDependency, "Add test distribution plugin as a dependency only.")
-	activateGradleCmd.Flags().IntVar(&activateGradleParams.TestDistro.ShardSize, "test-distribution-shard-size", activateGradleParams.TestDistro.ShardSize, "Shard size for test distribution plugin.")
+	ActivateGradleCmd.Flags().BoolVar(&activateGradleParams.TestDistro.Enabled, "test-distribution", activateGradleParams.TestDistro.Enabled, "Activate test distribution plugin for the provided app slug. Will override test-distribution-dep.")
+	ActivateGradleCmd.Flags().BoolVar(&activateGradleParams.TestDistro.JustDependency, "test-distribution-dep", activateGradleParams.TestDistro.JustDependency, "Add test distribution plugin as a dependency only.")
+	ActivateGradleCmd.Flags().IntVar(&activateGradleParams.TestDistro.ShardSize, "test-distribution-shard-size", activateGradleParams.TestDistro.ShardSize, "Shard size for test distribution plugin.")
 }
 
 func ActivateGradleCmdFn(
