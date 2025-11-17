84c8375f9c3fb93acc383ca899de42b2cae8aaee
Balázs Hajagos <balazs.hajagos@bitrise.io>
2025-11-17 11:47:26 +0100

    Find CLI module (#193)
    * Find CLI module even if it's a dep
    
    * Fix formatting

--- internal/config/common/cli_version.go
+++ internal/config/common/cli_version.go
@@ -2,6 +2,8 @@ package common
 
 import (
 	"runtime/debug"
+	"slices"
+	"strings"
 
 	"github.com/bitrise-io/go-utils/v2/log"
 )
@@ -14,5 +16,17 @@ func GetCLIVersion(logger log.Logger) string {
 		return "unknown"
 	}
 
-	return bi.Main.Version
+	// Find the bitrise-build-cache-cli module in the build info.
+	// If ran from source, it will be the main module.
+	// If ran from a step, it will be a dependency module.
+	modules := []*debug.Module{&bi.Main}
+	modules = append(modules, bi.Deps...)
+	idx := slices.IndexFunc(modules, func(c *debug.Module) bool { return strings.Contains(c.Path, "bitrise-build-cache-cli") })
+	if idx == -1 || idx >= len(modules) {
+		logger.Infof("Failed to find bitrise-build-cache-cli module in build info")
+
+		return "unknown"
+	}
+
+	return modules[idx].Version
 }
