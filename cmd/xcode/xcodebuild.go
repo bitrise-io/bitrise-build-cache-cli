98721ef0591887e7dc1406a02690105fcd9fe59d
Zsolt Marta <zsolt.marta@bitrise.io>
2025-11-19 12:39:52 +0100

    fix: Close file explicitly after context cancel

--- cmd/xcode/xcodebuild.go
+++ cmd/xcode/xcodebuild.go
@@ -225,11 +225,14 @@ func XcodebuildCmdFn(
 		}
 
 		if !config.Silent {
+			logger.Debugf("Streaming proxy logs...")
 			proxyCtx, cancel := context.WithCancel(ctx)
 			defer cancel()
 			go func() {
 				if err := streamProxyLogs(proxyCtx, invocationID, logger, utils.DefaultOsProxy{}); err != nil {
 					logger.Errorf("Failed to stream proxy logs: %v", err)
+				} else {
+					logger.Debugf("Proxy logs stream closed")
 				}
 			}()
 		}
@@ -434,6 +437,7 @@ func streamProxyLogs(ctx context.Context, invocationID string, logger log.Logger
 
 			return fmt.Errorf("failed to open proxy log file: %w", err), false
 		}
+		logger.TDebugf("Streaming proxy log file: %s", proxyLogFile)
 
 		return nil, true
 	})
@@ -441,17 +445,20 @@ func streamProxyLogs(ctx context.Context, invocationID string, logger log.Logger
 		return fmt.Errorf("failed to open proxy log file after retries: %w", err)
 	}
 
-	defer func(f *os.File) {
+	go func() {
+		<-ctx.Done()
+		logger.Debugf("Context done, closing log file")
 		err := f.Close()
 		if err != nil {
-			logger.Errorf("failed to close log file: %v", err)
+			logger.Errorf("Failed to close log file: %v", err)
 		}
-	}(f)
+	}()
 
 	r := bufio.NewReader(f)
 	for {
 		select {
 		case <-ctx.Done():
+			logger.Debugf("Context done, exiting streamProxyLogs")
 			return nil
 		default:
 		}
