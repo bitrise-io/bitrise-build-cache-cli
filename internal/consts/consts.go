97b0b3635f2e33234b292eb478f509360d59ab58
Bitrise CI <noreply@bitrise.io>
2026-04-22 13:03:05 +0000

    feat: update plugins to release

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -16,7 +16,7 @@ const (
 	BitriseWebsiteBaseURL = "https://app.bitrise.io"
 
 	// Gradle Remote Build Cache related consts
-	GradleRemoteBuildCachePluginDepVersion = "1.3.2"
+	GradleRemoteBuildCachePluginDepVersion = "1.3.3"
 
 	// Gradle Analytics related consts
 	GradleAnalyticsPluginDepVersion = "2.6.0"
